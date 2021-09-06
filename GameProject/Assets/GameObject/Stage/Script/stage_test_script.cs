6b295c71a90bc0753d4cd7d0df8dec0772ede6d5
いぶき <81256126+ibukianago@users.noreply.github.com>
2021-09-06 15:24:19 +0900

    Add_Moc_Stage1

--- GameProject/Assets/GameObject/Stage/Script/stage_test_script.cs
+++ GameProject/Assets/GameObject/Stage/Script/stage_test_script.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class stage_test_script : MonoBehaviour
 {
+    public int x;
+    public int y;
+    public int z;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        x = 0; y = 180; z = 0;
     }
 
     // Update is called once per frame
@@ -15,7 +21,7 @@ public class stage_test_script : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Return))
         {
-            transform.Rotate(new Vector3(0, 180, 0));
+            transform.Rotate(new Vector3(x, y, z));
         }
 
     }
