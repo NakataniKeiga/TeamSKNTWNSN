ae38521db3c3c59f5eef14f0cb4e49c65e01f8fe
いぶき <81256126+ibukianago@users.noreply.github.com>
2021-09-13 15:42:50 +0900

    Add_Goal

---
+++ GameProject/Assets/GameObject/Stage/Script/MocScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MocScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SceneManager.LoadScene("MocStage1",LoadSceneMode.Additive);
+        //SceneManager.LoadScene("UIScene", LoadSceneMode.Additive);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+
+        //    SceneManager.LoadScene("MocStage1", LoadSceneMode.Additive);
+        //    var currntScene = SceneManager.GetActiveScene();
+        //    Debug.Log(currntScene);
+        //    SceneManager.UnloadSceneAsync("MocStage2", UnloadSceneOptions.None);
+        //}
+
+    }
+}
