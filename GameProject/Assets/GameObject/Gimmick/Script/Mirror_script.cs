13d262f7e92a6a8f7d3dd51f546e134bddc98737
smdsyt1005 <81272719+smdsyt1005@users.noreply.github.com>
2021-09-16 12:32:12 +0900

    Add mirror script

--- GameProject/Assets/GameObject/Gimmick/Script/Mirror_script.cs
+++ GameProject/Assets/GameObject/Gimmick/Script/Mirror_script.cs
@@ -5,19 +5,80 @@ using UnityEngine;
 public class Mirror_script : MonoBehaviour
 {
 
-    private Vector3 Rot;
-    public float ADD_ROT = 0.02f;
+    private Vector3 Start_Rot;
+    private bool Hit_flg;
+    public float ADD_ROT_X = 0.00f;
+    public float ADD_ROT_Y = 0.02f;
+    public float ADD_ROT_Z = 0.00f;
+
+    public enum ROTATE_TAG
+    {
+        UP,
+        DOWN,
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Start_Rot = transform.localEulerAngles;
+        Hit_flg = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rot.y += ADD_ROT;
-        transform.Rotate(new Vector3(ADD_ROT, 0, 0));
+        if (Hit_flg == true)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Debug.Log("関数を呼びます");
+                MirrorRotate(ROTATE_TAG.UP);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Debug.Log("関数を呼びます");
+                MirrorRotate(ROTATE_TAG.DOWN);
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                Debug.Log("関数を呼びます");
+                transform.localEulerAngles = Start_Rot;
+            }
+        }
+
+    }
+
+    void MirrorRotate(ROTATE_TAG tag)
+    {
+        if (tag == ROTATE_TAG.UP)
+        {
+            transform.Rotate(new Vector3(ADD_ROT_X, ADD_ROT_Y, ADD_ROT_Z));
+            Debug.Log("上に向けます");
+        }
+        else if (tag == ROTATE_TAG.DOWN)
+        {
+            transform.Rotate(new Vector3((-1 * ADD_ROT_X), (-1 * ADD_ROT_Y), (-1 * ADD_ROT_Z)));
+            Debug.Log("下に向けます");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("プレイヤーがあたりました。");
+            Hit_flg = true;
+
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("プレイヤーが離れました。");
+            Hit_flg = false;
+        }
     }
 }
