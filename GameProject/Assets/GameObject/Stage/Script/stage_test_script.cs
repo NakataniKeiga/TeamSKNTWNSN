abbdc3ac8dcb2ed3e25d8322ca3e962fa50bac49
いぶき <81256126+ibukianago@users.noreply.github.com>
2021-09-17 11:52:58 +0900

    add_state

--- GameProject/Assets/GameObject/Stage/Script/stage_test_script.cs
+++ GameProject/Assets/GameObject/Stage/Script/stage_test_script.cs
@@ -8,21 +8,49 @@ public class stage_test_script : MonoBehaviour
     public int y;
     public int z;
 
-
+    public bool isLight_Flg;
+    public float count;
 
     // Start is called before the first frame update
     void Start()
     {
         x = 0; y = 180; z = 0;
+
+        count = 0;
+        isLight_Flg = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(count > 0)
         {
-            transform.Rotate(new Vector3(x, y, z));
+            count -= 1 * Time.deltaTime;
+            Debug.Log("ÇPÇ¬å∏ÇÁÇµÇΩ");
         }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                transform.Rotate(new Vector3(x, y, z));
+
+                if(isLight_Flg)
+                {
+                    isLight_Flg = false;
+                    Debug.Log("åªé¿èÛë‘Ç÷");
+                }
+                else
+                {
+                    isLight_Flg = true;
+                    Debug.Log("ÉâÉCÉgèÛë‘Ç÷");
+
+                }
+
+                count = 3;
+            }
+        }
+
+
 
     }
 }
