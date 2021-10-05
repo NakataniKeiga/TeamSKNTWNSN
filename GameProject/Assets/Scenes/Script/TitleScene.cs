f2e532104f8cc634aba0778cd018d7f54bffc0c2
nakamura1022 <1909421@s.asojuku.ac.jp>
2021-10-05 12:35:46 +0900

    AddSceneManagement
    タイトルからプレイシーンと、リザルドからタイトルに戻る仮スクリプトを追加しました。

---
+++ GameProject/Assets/Scenes/Script/TitleScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; /// シーン切り替えに必要
+
+public class TitleScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // エンターキーでPlayシーンへ(今は仮で)
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            // シーン切り替えにLoadScene関数を使う
+            SceneManager.LoadScene("PlayScene");
+        }
+    }
+}
