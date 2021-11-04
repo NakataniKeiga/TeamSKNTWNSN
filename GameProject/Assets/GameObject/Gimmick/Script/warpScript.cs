5a98a7ceca86e0b4dda1dc1f831b03252fd2ffa2
smdsyt1005 <81272719+smdsyt1005@users.noreply.github.com>
2021-11-05 03:14:29 +0900

    add all

--- GameProject/Assets/GameObject/Gimmick/Script/warpScript.cs
+++ GameProject/Assets/GameObject/Gimmick/Script/warpScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class warpScript : MonoBehaviour
 {
+
+    public GameObject next_port;
+    private Vector3 next_pos;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("プレイヤーにあたった");
+            if (Input.GetButtonDown("action_joy"))
+            {
+                Debug.Log("ボタンが押された");
+                next_pos = next_port.GetComponent<Transform>().position;
+                other.gameObject.GetComponent<Transform>().position = next_pos;
+                Debug.Log("座標更新");
+            }
+        }
+    }
+
 }
