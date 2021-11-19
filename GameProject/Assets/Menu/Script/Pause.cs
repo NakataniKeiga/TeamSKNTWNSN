2107125b8c50af92b36c51673c5f572a7714a59d
ToaYoshida-1023 <81272872+ToaYoshida-1023@users.noreply.github.com>
2021-11-19 15:12:36 +0900

    a

--- GameProject/Assets/Menu/Script/Pause.cs
+++ GameProject/Assets/Menu/Script/Pause.cs
@@ -7,29 +7,47 @@ using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
+    private GameObject MiniMap;
+    private GameObject Menu2D;
+    private GameObject Option2D;
+
     // Start is called before the first frame update
     void Start()
     {
+        MiniMap = GameObject.Find("MiniMap_Canvas");
+        Menu2D = GameObject.Find("Menu_testCanvas");
+        Option2D = GameObject.Find("Option_testCanvas");
 
+        //UIを非表示に
+        Menu2D.gameObject.SetActive(false);
+        Option2D.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         //Mキーで「ゲーム時間一時停止」
-        if(Input.GetKey(KeyCode.M))
+        if (Input.GetKey(KeyCode.M))
         {
             Time.timeScale = 0;
+
+            //メニュー画面表示
+            Menu2D.gameObject.SetActive(true);
+            //Option2D.gameObject.SetActive(true);
         }
 
         //Nキーで「ゲーム時間再開」
         if (Input.GetKey(KeyCode.N))
         {
             Time.timeScale = 1;
+
+            //メニュー画面削除
+            Menu2D.gameObject.SetActive(false);
+            Option2D.gameObject.SetActive(false);
         }
 
         //Hキーで「タイトルシーンに遷移」
-        if(Input.GetKey(KeyCode.H))
+        if (Input.GetKey(KeyCode.H))
         {
             SceneManager.LoadScene("TitleScene");
         }
@@ -39,5 +57,35 @@ public class Pause : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
+        //Kキーで「ミニマップを表示→非表示」
+        if (Input.GetKey(KeyCode.K))
+        {
+            MiniMap.gameObject.SetActive(false);
+        }
+
+        //Lキーで「ミニマップ非表示→表示」
+        if (Input.GetKey(KeyCode.L))
+        {
+            MiniMap.gameObject.SetActive(true);
+        }
+
+        //Escキーで「ゲーム終了」--------------------------
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Quit();
+        }
+
+        void Quit()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+            #endif
+        }
+        //--------------------------------------------------
     }
+
 }
+
