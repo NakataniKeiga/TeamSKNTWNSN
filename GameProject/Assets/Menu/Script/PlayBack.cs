190783ac130766f0323f897a9165c708ca9d3d62
ToaYoshida-1023 <81272872+ToaYoshida-1023@users.noreply.github.com>
2021-11-26 15:11:28 +0900

    a

---
+++ GameProject/Assets/Menu/Script/PlayBack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBack : MonoBehaviour
+{
+    private GameObject Menu2D;
+    private GameObject Option2D;
+    private GameObject MenuBack2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu2D = GameObject.Find("Menu_Canvas");
+        Option2D = GameObject.Find("Option_Canvas");
+        MenuBack2D = GameObject.Find("Menu_BackCanvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayBack_Move()
+    {
+        Menu2D.gameObject.SetActive(false);
+        Option2D.gameObject.SetActive(false);
+        MenuBack2D.gameObject.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+}
