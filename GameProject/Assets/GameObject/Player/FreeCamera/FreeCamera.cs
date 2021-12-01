95ea4f2e7dc36d7709fb5f9a4475495d2aee729e
smdsyt1005 <81272719+smdsyt1005@users.noreply.github.com>
2021-12-01 17:30:39 +0900

    add

--- GameProject/Assets/GameObject/Player/FreeCamera/FreeCamera.cs
+++ GameProject/Assets/GameObject/Player/FreeCamera/FreeCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class FreeCamera : MonoBehaviour
 {
@@ -15,42 +16,40 @@ public class FreeCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    //    float h;
-    //    float v;
+        float h = 0.0f;
+        float v = 0.0f;
 
-    //    // read inputs
-    //    if (Input.GetAxis("Horizontal") != 0)
-    //    {
-    //        h = CrossPlatformInputManager.GetAxis("Horizontal");
-    //    }
-    //    else
-    //    {
-    //        h = CrossPlatformInputManager.GetAxis("joystick_L_H");
-    //    }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            h = CrossPlatformInputManager.GetAxis("Horizontal");
+        }
+        else
+        {
+            h = CrossPlatformInputManager.GetAxis("pov_L_H");
 
-    //    if (Input.GetAxis("Vertical") != 0)
-    //    {
-    //        v = CrossPlatformInputManager.GetAxis("Horizontal");
-    //    }
-    //    else
-    //    {
-    //        v = CrossPlatformInputManager.GetAxis("joystick_L_V");
-    //        //v = 0.0f;
-    //    }
+            move_pos.x = transform.position.x;
+            move_pos.y = h * transform.position.y;
+            move_pos.z = transform.position.z;
 
-    //    bool crouch = Input.GetKey(KeyCode.C);
+            transform.position = move_pos;
+        }
 
-    //    // calculate move direction to pass to character
-    //    if (m_Cam != null)
-    //    {
-    //        // calculate camera relative direction to move:
-    //        m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-    //        m_Move = v * m_CamForward + h * m_Cam.right;
-    //    }
-    //    else
-    //    {
-    //        // we use world-relative directions in the case of no main camera
-    //        m_Move = v * Vector3.forward + h * Vector3.right;
-    //    }
-    //}
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        }
+        else
+        {
+            v = CrossPlatformInputManager.GetAxis("pov_L_V");
+
+            move_pos.x = v * transform.position.x;
+            move_pos.y = transform.position.y;
+            move_pos.z = transform.position.z;
+
+            transform.position = move_pos;
+        }
+
+
+    }
 }
