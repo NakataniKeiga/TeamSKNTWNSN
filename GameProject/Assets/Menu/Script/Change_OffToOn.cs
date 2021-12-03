59f2153d68d87fb521c31ad3189778dd1bae09bb
ToaYoshida-1023 <81272872+ToaYoshida-1023@users.noreply.github.com>
2021-12-03 15:14:53 +0900

    EditMenu_1203

---
+++ GameProject/Assets/Menu/Script/Change_OffToOn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//í«â¡ÇµÇΩÇ‚Ç¬Å´
+using UnityEngine.UI;
+
+public class Change_OffToOn : MonoBehaviour
+{
+    public Sprite _on;
+    public Sprite _off;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeOffToOn_Move()
+    {
+        //if (ImageSwitch == true) { image.sprite = _off; ImageSwitch = false; };
+        //if (ImageSwitch == false) { image.sprite = _on; ImageSwitch = true; };
+    }
+}
